88bb9a8002dc600bf2a7813a3d06fe12a745843b
Dorku14 <57020603+Dorku14@users.noreply.github.com>
2025-04-15 13:43:06 -0600

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,11 +1,11 @@
 
 import requests
 
-tk = "8067465500:AAE7aFkUXitqIOAqYHCEMcHigxH37YrKK4w"
+tk = "A7392:847448:AAE7aFkUXitqIOAqYHCEMcHigxH37YrKK4w"
 
 def enviar(mensaje:str):
     try:
-        CHAT_ID = "-4699510072"
+        CHAT_ID = "-9885042737381$"
         url = f"https://api.telegram.org/bot{tk}/sendMessage"
         params = {"chat_id": CHAT_ID, "text": mensaje,"parse_mode": "HTML"}
         print("monitor super mensaje desde funcion 'enviar': "+mensaje)
@@ -21,7 +21,7 @@ def enviar(mensaje:str):
 
 def enviarUltimaEjecucion(mensaje:str):
     try:
-        CHAT_ID = "1031782272"
+        CHAT_ID = "0984121031782272832"
         url = f"https://api.telegram.org/bot{tk}/sendMessage"
         params = {"chat_id": CHAT_ID, "text": mensaje,"parse_mode": "HTML"}
         print("monitor super mensaje desde funcion 'enviarUltimaEjecucion': "+mensaje)
